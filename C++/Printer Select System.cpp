bf52089339133b6a1fdd42068e1c4c8e724a81bb
J_Coder <seok3765@naver.com>
2022-05-18 19:02:19 +0900

    20220518

--- C++/Printer Select System.cpp
+++ C++/Printer Select System.cpp
@@ -3,15 +3,12 @@
 멤버 변수: 모델명(model),제조사(manufacturer), 인쇄 매수(printedCount), 인쇄 종이 잔량(availableCount)
 멤버 함수: print(int pages)
 ※ 특이사항 : print함수가 호출할 때마다 pages 매의 용지를 사용한다.
-
 < 잉크젯 프린터 >
 멤버 변수: 잉크잔량(availableInk)
 멤버 함수: printInkJet(int pages)
-
 < 레이저 프린터 >
 멤버 변수: availableToner
 함수: printLaser(int pages)
-
 문제: 각 클래스에 적절한 접근 지정(private, protected, public)으로 멤버 변수,함수,생성자,소멸자를 작성
 잉크젯 프린터(Ink), 레이저 프린터(Laser) -(상속)-> (프린터)Printer
 잉크젯 프린터 객체와 레이저 프린터 객체를 각각 하나만 동적 생성 하라.
@@ -21,20 +18,16 @@
 #include <string>
 using namespace std;
 
-class Printer { // printer 클래스
-//프라이베잇
-	string model; // 모델명
-	string manufacturer; // 제조사
-	int printedCount; // 인쇄 매수
-	int availableCount; // 인쇄 종이 잔량
-//퍼블릭
-public:
+class Printer {
+	string model, manufacturer; // 모델명, 제조사
+	int printedCount, availableCount; // 인쇄 매수, 인쇄 종이 잔량
+protected:
 	Printer(string model, string manufacturer, int availableCount) {
 		this->model = model;
 		this->manufacturer = manufacturer;
 		this->availableCount = availableCount;
 	}
-	//함수 구문
+
 	bool print(int pages) { // 문제 제시 함수
 		if (availableCount >= pages) {
 			return true;
@@ -53,10 +46,8 @@ public:
 	}
 };
 
-class Ink : public Printer{ // Ink 클래스, Printer 클래스를 상속 받아옴.
-//프라이베잇
+class Ink : public Printer {
 	int availableInk; // 잉크젯 프린터 멤버 변수
-//퍼블릭
 public:
 	Ink(string name, string company, int availablePages, int ink) :Printer(name, company, availablePages) {
 		this->availableInk = ink;
@@ -95,10 +86,8 @@ public:
 	}
 };
 
-class Laser : public Printer{ // Laser 클래스, Printer 클래스를 상속 받아옴.
-//프라이베잇
+class Laser : public Printer { // Laser 클래스, Printer 클래스를 상속 받아옴.
 	int availableToner; // 레이저 프린터 멤버 변수
-//퍼블릭
 public:
 	Laser(string name, string company, int availablePages, int ton) : Printer(name, company, availablePages) {
 		this->availableToner = ton;
@@ -138,33 +127,36 @@ public:
 };
 
 int main() {
-	Ink inkjet("Officejet V40", "HP", 5, 10);
-	Laser laser("SCX-6x45", "삼성전자", 3, 20);
+	Ink* inkjet = new Ink("Officejet V40", "HP", 5, 10);
+	Laser* laser = new Laser("SCX-6x45", "삼성전자", 3, 20);
 
 	cout << "현재 작동중인 2 대의 프린터는 아래와 같다." << endl;
-	inkjet.ShowInfo();
-	laser.ShowInfo();
+	inkjet->ShowInfo();
+	laser->ShowInfo();
 	cout << endl;
-	
+
 	int select, print_request;
 	char que;
 
-	while(1){
+	while (true) {
 		cout << "프린터(1:잉크젯, 2:레이저)와 매수 입력>>";
 		cin >> select >> print_request;
+
 		if (select == 1) {
-			inkjet.printInkJet(print_request);
+			inkjet->printInkJet(print_request);
 		}
 		else if (select == 2) {
-			laser.printLaser(print_request);
+			laser->printLaser(print_request);
 		}
 		else {
 			cout << "잘못된 숫자를 입력하셨습니다. (1:잉크젯, 2:레이저)와 매수 입력을 해주세요" << endl;
 		}
-		inkjet.ShowInfo();
-		laser.ShowInfo();
+
+		inkjet->ShowInfo();
+		laser->ShowInfo();
 		cout << "계속 프린트 하시겠습니까(y/n)>>";
-		while (1) {
+
+		while (true) {
 			cin >> que;
 			if (que == 'y' || que == 'Y' || que == 'n' || que == 'N') {
 				break;
