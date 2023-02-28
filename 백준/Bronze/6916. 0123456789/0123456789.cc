ce1b499870e74af141840f067c5afc28cae2fd3d
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-02-28 18:41:54 +0900

    [Bronze IV] Title: 0123456789, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6916. 0123456789/0123456789.cc
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int num;
+	cin >> num;
+	switch (num)
+	{
+	case 0:
+		cout << " * * *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << " * * *\n";
+		break;
+	case 1:
+		cout << "\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "\n";
+		break;
+	case 2:
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << " * * *\n";
+		cout << "*\n";
+		cout << "*\n";
+		cout << "*\n";
+		cout << " * * *\n";
+		break;
+	case 3:
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << " * * *\n";
+		break;
+	case 4:
+		cout << "\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "\n";
+		break;
+	case 5:
+		cout << " * * *\n";
+		cout << "*\n";
+		cout << "*\n";
+		cout << "*\n";
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << " * * *\n";
+		break;
+	case 6:
+		cout << " * * *\n";
+		cout << "*\n";
+		cout << "*\n";
+		cout << "*\n";
+		cout << " * * *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << " * * *\n";
+		break;
+	case 7:
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "\n";
+		break;
+	case 8:
+		cout << " * * *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << " * * *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << " * * *\n";
+		break;
+	case 9:
+		cout << " * * *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << "*     *\n";
+		cout << " * * *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << "      *\n";
+		cout << " * * *\n";
+		break;
+	}
+}
