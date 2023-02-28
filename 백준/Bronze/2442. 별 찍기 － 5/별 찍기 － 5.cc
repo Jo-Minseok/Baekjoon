25227471f39461254a061ceb922c86bd41340f62
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-02-28 18:27:37 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		for (int j = num-1; j > i; j--) {
+			printf(" ");
+		}
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
