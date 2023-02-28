ac9a25171426a462f4875089778280b096bca392
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-02-28 18:55:22 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int count;
+	cin >> count;
+	for (int j = 0; j < count; j++) {
+		for (int i = 0; i < count; i++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int j = 0; j < count; j++) {
+		for (int i = 0; i < count; i++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int j = 0; j < count; j++) {
+		for (int i = 0; i < count; i++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int j = 0; j < count; j++) {
+		for (int i = 0; i < count; i++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int j = 0; j < count; j++) {
+		for (int i = 0; i < count; i++) {
+			cout << "@@@@@";
+		}
+		cout << "\n";
+	}
+}
