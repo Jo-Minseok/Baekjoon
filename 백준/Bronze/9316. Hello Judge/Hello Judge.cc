8918e6731e8ed02152fb380458c25fc68f96d871
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-02-28 18:42:19 +0900

    [Bronze IV] Title: Hello Judge, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int count = 0;
+
+    cin >> count;
+
+    for(int i=1;i<=count;i++){
+
+        cout << "Hello World, Judge " << i <<"!" << endl;
+
+    }
+
+}
+
