5d0790e3af477b328ca553ec6fb1be76d6dc2ffd
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-03-05 18:30:22 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -1,16 +1,6 @@
 #include <iostream>
 using namespace std;
-int sum(int a, int b) {
-	if (a == 0) {
-		return b;
-	}
-	else if (b == 1) {
-		return 1;
-	}
-	else {
-		return sum(a - 1, b) + sum(a,b-1) ;
-	}
-}
+int arr[15][14];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -18,8 +8,21 @@ int main() {
 	cout.tie(nullptr);
 	int test_case,k,n;
 	cin >> test_case;
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 14; j++) {
+			if (i == 0) {
+				arr[i][j] = j+1;
+			}
+			else if (j == 0) {
+				arr[i][j] = 1;
+			}
+			else {
+				arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+			}
+		}
+	}
 	for (int i = 0; i < test_case; i++) {
 		cin >> k >> n;
-		cout << sum(k, n) << "\n";;
+		cout << arr[k][n-1] << "\n";
 	}
 }
