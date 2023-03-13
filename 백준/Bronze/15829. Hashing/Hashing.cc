c8118e2d432c6f97820a60d8ad374714398b883d
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-03-13 23:48:53 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -11,7 +11,11 @@ int main() {
 	cin >> test_case;
 	cin >> alpha;
 	for (int i = 0; i < alpha.length(); i++) {
-		result += (alpha[i] - 'a' + 1) * pow(31, i);
+		unsigned long long num = 1;
+		for (int j = 0; j < i; j++) {
+			num *= 31;
+		}
+		result += (alpha[i] - 'a' + 1) * num;
 	}
-	std::cout << result;
+	cout << result%1234567891;
 }
