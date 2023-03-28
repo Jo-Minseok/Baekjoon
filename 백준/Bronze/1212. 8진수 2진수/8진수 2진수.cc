e5731ab7594c6cf2563780f0ec5157de0e89bbbb
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-03-28 16:35:05 +0900

    [Bronze II] Title: 8진수 2진수, Time: 36 ms, Memory: 5544 KB -BaekjoonHub

---
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string num;
+	string result;
+	cin >> num;
+	if (num == "0") {
+		cout << 0;
+		return 0;
+	}
+	for (int i = 0; i < num.length(); i++) {
+		int tmp = num[i] - '0';
+		bitset<3> list(tmp);
+		result += list.to_string();
+	}
+	int index = 0;
+	for (int i = 0; i < result.length(); i++,index++) {
+		if (result[i] != '0') {
+			break;
+		}
+	}
+	for (int i = index; i < result.length(); i++) {
+		cout << result[i];
+	}
+}
