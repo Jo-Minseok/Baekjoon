b601e2ccb0e9e12d14262d1b78289b3f5c49986c
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-05-23 21:01:56 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-int main() {
-	int first, second;
-	while (scanf("%d %d", &first, &second)!=EOF) {
-		if (first != 0 && second != 0) {
-			printf("%d\n", first + second);
-		}
-		else {
-			break;
-		}
-	}
-	return 0;
+int main(){
+    int a,b, i, result;
+
+    while(1){
+
+        scanf("%d %d", &a, &b);
+        result=a+b;
+        
+        if(result>0){
+            printf("%d\n", result);
+        }
+        else{
+            break;
+        }
+
+    }
+    return 0;
 }
