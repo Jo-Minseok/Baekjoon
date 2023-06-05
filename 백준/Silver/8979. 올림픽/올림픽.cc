147a7d601f58cac994fc9ed3925f5b6718010dd5
J_Coder <99482796+Jo-Minseok@users.noreply.github.com>
2023-06-05 16:38:59 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -2,76 +2,66 @@
 #include <algorithm>
 using namespace std;
 
-class Country{
-    public:
+class Country {
+public:
     int num, gold, silver, bronze;
-    void get(){
+    void get() {
         cin >> this->num >> this->gold >> this->silver >> this->bronze;
     }
 };
 
-bool comp(Country a, Country b){
-    if(a.gold > b.gold){
+bool comp(Country a, Country b) {
+    if (a.gold > b.gold) {
         return true;
     }
-    else if(a.gold == b.gold){
-        if(a.silver > b.silver){
+    else if (a.gold == b.gold) {
+        if (a.silver > b.silver) {
             return true;
         }
-        else if(a.silver == b.silver){
-            if(a.bronze > b.bronze){
+        else if (a.silver == b.silver) {
+            if (a.bronze > b.bronze) {
                 return true;
             }
-            else if(a.bronze == b.bronze){
-                return false;
-            }
-            else{
-                return false;
-            }
-        }
-        else{
-            return false;
         }
     }
-    else{
-        return false;
-    }
+    return false;
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    Country list[1000];
+    Country list[1001];
     int country_count, want_country;
-    int equal=0,prize = 1;
+    int equal = 0, prize = 1;
     cin >> country_count >> want_country;
-    for(int i=0;i<country_count;i++){
+    for (int i = 0; i < country_count; i++) {
         list[i].get();
     }
-    sort(list, &list[country_count+1],comp);
+    sort(list, list+country_count, comp);
 
-    for(int i=0;i<country_count;i++){
-        if(list[i].num == want_country){
+    for (int i = 0; i < country_count; i++) {
+        if (list[i].num == want_country) {
             cout << prize;
             break;
         }
-        if(list[i].gold > list[i+1].gold){
+        if (list[i].gold > list[i + 1].gold) {
+            prize += equal + 1;
             equal = 0;
-            prize += equal+1;
         }
-        else if(list[i].gold == list[i+1].gold){
-            if(list[i].silver > list[i+1].silver){
-                prize += equal+1;
+        else if (list[i].gold == list[i + 1].gold) {
+            if (list[i].silver > list[i + 1].silver) {
+                prize += equal + 1;
+                equal = 0;
             }
-            else if( list[i].silver == list[i+1].silver){
-                if(list[i].bronze > list[i+1].bronze){
+            else if (list[i].silver == list[i + 1].silver) {
+                if (list[i].bronze > list[i + 1].bronze) {
+                    prize += equal + 1;
                     equal = 0;
-                    prize += equal+1;
                 }
-                else if(list[i].bronze == list[i+1].bronze){
-                    equal++; 
+                else if (list[i].bronze == list[i + 1].bronze) {
+                    equal++;
                 }
             }
         }
