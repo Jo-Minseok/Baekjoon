7d4815b48be7ed1654c0ceaad1da87e4acedeacf
J_Coder <seok3765@naver.com>
2024-06-21 02:33:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,1 @@
-function solution(n) {
-    var answer = []
-    for(let i = 0;i<n;i++){
-        i & 1 ? answer.push("박") : answer.push("수");
-    }
-    return answer.join("");
-}
+const solution = (n) => "수박".repeat(n).slice(0,n);
